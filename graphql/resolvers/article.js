79f9ed34e7237dbca0f8572e4d43e90e63a8a52d
John Samuel <jsamchineme@gmail.com>
2019-06-28 09:11:34 +0100

    Setup with query loader

--- graphql/resolvers/article.js
+++ graphql/resolvers/article.js
@@ -1,39 +1,37 @@
+import queryLoader from '../queryLoaderConfig';
+
+
 export default {
   Query: {
-    articles: async (parent, args, { models }) => {
-      const articles = await models.Article.findAll();
+    articles: async (parent, args, { models }, info) => {
+      const { Article } = models;
+      // query options prepare attributes and associated model includes
+      const queryOptions = queryLoader.getFindOptions({ model: Article, info });
+
+      const articles = await Article.findAll(queryOptions);
       return articles;
     },
-    article: async (parent, { id }, { models }) => {
-      const article = await models.Article.findByPk(id);
+    article: async (parent, { id }, { models }, info) => {
+      const { Article } = models;
+      // query options prepare attributes and associated model includes
+      const queryOptions = queryLoader.getFindOptions({ model: Article, info });
+
+      const article = await Article.findByPk(id, queryOptions);
       return article;
     },
   },
   Article: {
-    owner: parent => parent.getOwner(),
-    category: parent => parent.getCategory(),
-    comments: parent => parent.getComments(),
+    owner: (parent) => {
+      const { owner } = parent;
+      return owner;
+    },
+    category: (parent) => {
+      const { category } = parent;
+      return category;
+    },
+    comments: (parent) => {
+      const { comments } = parent;
+      return comments;
+    }
   },
-  Mutation: {
-    createArticle: async (parent, args, { me, models: { Article } }) => {
-      args.slug = args.title.replace(/\s+/g, '-').toLowerCase();
-      args.authorId = me.id;
-
-      const newArticle = await Article.create(args);
-      return newArticle;
-    },
-    deleteArticle: async (parent, { id }, { models: { Article } }) => {
-      const article = Article.findByPk(id);
-      if (!article) {
-        return false;
-      }
-
-      const result = await article.destroy();
-      if (!result) {
-        return false;
-      }
-
-      return true;
-    },
-  }
 };
--- graphql/resolvers/category.js
+++ graphql/resolvers/category.js
@@ -1,34 +1,26 @@
+import queryLoader from '../queryLoaderConfig';
+
 export default {
   Query: {
-    categories: async (parent, args, { models }) => {
-      const categories = await models.Category.findAll();
+    categories: async (parent, args, { models }, info) => {
+      const { Category } = models;
+      const queryOptions = queryLoader.getFindOptions({ model: Category, info });
+
+      const categories = await Category.findAll(queryOptions);
       return categories;
     },
-    category: async (parent, { id }, { models }) => {
-      const category = await models.Category.findByPk(id);
+    category: async (parent, { id }, { models }, info) => {
+      const { Category } = models;
+      const queryOptions = queryLoader.getFindOptions({ model: Category, info });
+
+      const category = await Category.findByPk(id, queryOptions);
       return category;
     }
   },
   Category: {
-    articles: parent => parent.getArticles()
+    articles: async (parent) => {
+      const { articles } = parent;
+      return articles;
+    }
   },
-  Mutation: {
-    createCategory: async (parent, args, { models: { Category } }) => {
-      const newCategory = await Category.create(args);
-      return newCategory;
-    },
-    deleteCategory: async (parent, { id }, { models: { Category } }) => {
-      const category = await Category.findByPk(id);
-      if (!category) {
-        return false;
-      }
-
-      const result = await category.destroy();
-      if (!result) {
-        return false;
-      }
-
-      return true;
-    },
-  }
 };
